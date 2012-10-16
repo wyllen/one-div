598f45c009555c7e7d57922600d1914810ed5fbb
Vincent DURAND <wyllen44@gmail.com>
2012-10-16 12:15:28 +0200

    4

--- wp-content/themes/one-div/index.php
+++ wp-content/themes/one-div/index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 	<div id="content">
 	<?php get_sidebar();
-		the_content();
-	?>			
+		if(have_posts()) : while(have_posts()) : the_post();
+		the_content();	
+		endwhile;endif;
+		?>			
 		</div>
 <?php get_footer(); ?>
