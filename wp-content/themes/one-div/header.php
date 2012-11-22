7be3847e12be1a35c061fe29e186e979a4c680d5
Vincent DURAND <wyllen44@gmail.com>
2012-11-22 01:10:38 +0100

    20

--- wp-content/themes/one-div/header.php
+++ wp-content/themes/one-div/header.php
@@ -8,9 +8,25 @@
 	<title>One div - The single element HTML/CSS icon database</title>	
 		<?php wp_head(); ?>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/16x16.ico" /></head>
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/16x16.ico" />
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36511857-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <!--	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script> -->
 		<script src="<?php bloginfo('template_url'); ?>/js/jquery.zclip.min.js"></script> 
+	</head>
+
+		
 	
 <body>
 		<header id="header">
@@ -23,5 +39,9 @@
 		<div class="power"></div>		
 		<div class="login_ajax"><?php login_with_ajax() ?></div>
 		</nav>
-		<div class="clear"></div>
+		<div class="clear"></div>		
 		</header>
+	
+<script src="http://cdn.wibiya.com/Toolbars/dir_1297/Toolbar_1297007/Loader_1297007.js" type="text/javascript"></script><noscript><a href="http://www.wibiya.com/">Web Toolbar by Wibiya</a></noscript> 
+
+		<!-- AddThis Button BEGIN -->
